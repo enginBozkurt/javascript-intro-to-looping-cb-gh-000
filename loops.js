6a6e060aca498cb4e11fadfdd29b6d47e82499da
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-15 09:18:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,4 +9,21 @@ function forLoop(array) {
   }
   return array;
 }
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
 
